3fd5015af3fb7f8d70383fd90820f1c981484c44
Gursharan <gursharan.techwebers@gmail.com>
2026-05-11 15:33:31 +0530

    demo env

---
+++ env.ts
@@ -0,0 +1,9 @@
+WP_GRAPHQL_URL=http://chronicle/graphql
+
+NEXTAUTH_URL=http://localhost:3000
+NEXTAUTH_SECRET=randomsecret
+
+
+// both from https://console.cloud.google.com/
+GOOGLE_CLIENT_ID=
+GOOGLE_CLIENT_SECRET=
